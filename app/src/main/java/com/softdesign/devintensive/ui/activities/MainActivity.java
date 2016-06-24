23168f68064a2aebefd901272291c48bcfe57a56
hellmojo <energizr@mail.ru>
2016-06-25 05:08:35 +0800

    fix life circle

--- app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
@@ -1,26 +1,45 @@
 package com.softdesign.devintensive.ui.activities;
 
+import android.graphics.Color;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.softdesign.devintensive.R;
+import com.softdesign.devintensive.utils.CostantManager;
 
-public class MainActivity extends AppCompatActivity {
-    private static final String TAG = CostantManager.TAG_PREFIX +"MainActivity";
+public class MainActivity extends AppCompatActivity{
+    private static final String TAG = CostantManager.TAG_PREFIX + "MainActivity";
+
     /*Придумать и записать сюда javadoc
 */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Log.d(TAG, "onCreate");
+
+
+        if (savedInstanceState == null) {
+            //первый запуск активити
+        } else {
+            //активити уже запускалось
+        }
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d(TAG, "onCreate");
+
+        Log.d(TAG, "onStart");
     }
 
     @Override
@@ -52,4 +71,5 @@ public class MainActivity extends AppCompatActivity {
         super.onRestart();
         Log.d(TAG, "onRestart");
     }
+
 }
